8f526299bf6e535e37fbe71b310b31ac8d36754b
MMHK <sam@mixmedia.com>
2022-10-24 12:34:17 +0800

    replace config

---
+++ config/services.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'mm' => [
+        'shortlink' => [
+            'url' => 'https://api-ssl.bitly.com/v3/shorten',
+            'login' => 'bitly4mm',
+            'apiKey' => 'R_e58ca441b68d4bd996e8a1e88675cce9',
+        ],
+    ],
+
+    'github' => [
+        'webhook' => [
+            'projects-secret' => [
+                'github-notification' => env('GITHUB-NOTIFICATION_WEBHOOK_SECRET', ''),
+            ],
+        ],
+    ],
+];
